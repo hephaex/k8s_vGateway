a9f32d5e4738c72aec5d4f0ddf63ec3592164583
Mario Cho <hephaex@gmail.com>
2025-12-15 09:17:15 +0900

    chore: Bump version to 0.1.1

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -8,7 +8,7 @@ use clap::{Parser, Subcommand};
 #[derive(Parser, Debug)]
 #[command(name = "gateway-poc")]
 #[command(author = "hephaex@gmail.com")]
-#[command(version = "0.1.0")]
+#[command(version = "0.1.1")]
 #[command(about = "Test and compare 7 Gateway API implementations")]
 #[command(long_about = None)]
 pub struct Args {
